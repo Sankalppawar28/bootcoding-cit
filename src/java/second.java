e8a63fd17124dace12d761b5a4c44cbca3a38f21
Sankalppawar28 <sankalppawar2003@gmail.com>
2023-08-17 15:50:31 +0530

    java programs beginner

---
+++ src/java/second.java
@@ -0,0 +1,7 @@
+public class second {
+    public static void main(String[] args) {
+        int a=10;
+        int b=++a+ 10;
+        System.out.println(b);
+    }
+}
---
+++ src/java/third.java
@@ -0,0 +1,12 @@
+public class third {
+    public static void main(String[] args) {
+        int hours = 10;
+        int minutes = 30;
+        int seconds = minutes * 60;
+        int hourSeconds = hours * 80 * 60;
+
+        System.out.println("Hours to Seconds : " + hourSeconds);
+        System.out.println("Minutes to Seconds : " + seconds);
+
+    }
+}
